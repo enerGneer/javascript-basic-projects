7e8cc36a22a62e9d85ad0e425d84c00114304931
enerGneer <energneer@gmail.com>
2023-11-04 16:24:35 +0900

    Update scroll

--- 10-scroll/README.md
+++ 10-scroll/README.md
@@ -7,3 +7,17 @@
 - getBoundingClientRect()
 - slice method
 - window.scrollTo()
+
+### getBoundingClientRect()
+
+- 다이나믹하게 값을 가져올 수 있다.
+- 여기서는 links.getBoundingClientRect().height; 라고 썼으므로 height 을 가져옴
+
+- https://developer.mozilla.org/ko/docs/Web/API/Element/getBoundingClientRect
+
+### pageYOffset is deprecated?
+
+- 구형 브라우저 신경안써도 되면 scrollY 쓰고, 구형 브라우저까지 신경써야 한다면 pageYoffset을 쓴다.
+
+- https://stackoverflow.com/questions/44757869/window-pageyoffset-vs-window-scrolly-on-ie11
+- https://studyhardgogo.tistory.com/186
--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -4,10 +4,80 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
 
 // ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  // linksContainer.classList.toggle("show-links");
+  // 위의 코드도 나쁘진 않지만 다이나믹하게 해 주는 편이 좋다
+
+  const linksHeight = links.getBoundingClientRect().height;
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    // 토글을 연 경우
+    linksContainer.style.height = 0;
+  }
+});
 
 // ********** fixed navbar ************
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+  // 구형 브라우저 신경안써도 되면 scrollY 쓰고, 구형 브라우저까지 신경써야 한다면 pageYoffset을 쓴다
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+  // 맨위로 올라가는 버튼
+
+  if (scrollHeight > 500) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
 
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    // prevent default
+    e.preventDefault();
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    // 높이 계산하기
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains("fixed-nav");
+    let position = element.offsetTop - navHeight;
+    // navHeight을 빼주지 않으면 맨위로 올라가버림
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      // 햄버거 버튼으로 접혔을 때
+      position = position + containerHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    linksContainer.style.height = 0;
+    // 스크롤 시에 메뉴 닫아주기
+  });
+});
